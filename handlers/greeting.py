80310fe8575d9bec55d27dd3fd3003ec6d73297c
vitaliyzh92 <zh.vitaliy92@yandex.ru>
2023-12-22 23:54:03 +0300

    Оптимизация кода

--- handlers/greeting.py
+++ handlers/greeting.py
@@ -15,17 +15,7 @@ from messages.bonus_text import random_bon, bonus_post
 from messages.greeting_post import greeting_post  # Пояснение для пользователя FAG
 from system.dispatcher import dp, bot  # Подключение к боту и диспетчеру пользователя
 
-# Подключение к базе данных SQLite
-conn = sqlite3.connect('orders.db')
-cursor = conn.cursor()
-# Создаем таблицу, если она не существует
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, 
-                                                    first_name TEXT, last_name TEXT, username TEXT, date TEXT)''')
-# Создание таблицы
-cursor.execute('''CREATE TABLE IF NOT EXISTS users_bonus (user_key TEXT PRIMARY KEY, id INTEGER, full_name TEXT, 
-                                                          user_name TEXT, bonus TEXT, plase TEXT, FOREIGN KEY (id) 
-                                                          REFERENCES users (id))''')
-conn.commit()  # Сохраняем изменения
+
 
 
 @dp.message_handler(commands=['start'])
