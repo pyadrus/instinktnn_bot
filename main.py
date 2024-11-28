64a4ce6b5197298c963f8096f5d2b33a1a49b05b
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-11-28 21:33:10 +0300

    Миграция с aiogram 2 на aiogram 3.

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-from aiogram import executor
+import asyncio
+import logging
+import sys
+
+from loguru import logger
 
 from handlers.admin_handlers.admin_handlers import register_admin_handler
-from handlers.user_handlers.Dobrolyudova_handlers import register_dobrolyudova_handler
-from handlers.user_handlers.Llyicha_handlers import register_ilyich_handler
-from handlers.user_handlers.faq import faq_handlers
 from handlers.greeting import greeting_handler
+from handlers.user_handlers.dobrolyudova_handlers import register_dobrolyudova_handler
+from handlers.user_handlers.faq import faq_handlers
+from handlers.user_handlers.llyicha_handlers import register_ilyich_handler
 from handlers.user_handlers.moscow_faq import moscow_faq_handlers
+from system.dispatcher import bot
 from system.dispatcher import dp
 
-
-def main():
-    executor.start_polling(dp, skip_updates=True)
-    greeting_handler()
-    faq_handlers()
-    register_admin_handler()
-    moscow_faq_handlers()  # Регистрируем handlers для FAQ в Москве
-    register_dobrolyudova_handler()  # Регистрируем handlers для Dobrolyudova
-    register_ilyich_handler()  # Регистрируем handlers для Ilyich
+logger.add("logs/log.log", retention="1 days", enqueue=True)  # Логирование бота
 
 
-if __name__ == '__main__':
+async def main():
     try:
-        main()  # Запуск бота
+        await dp.start_polling(bot)
+
+        greeting_handler()
+        faq_handlers()
+        register_admin_handler()
+        moscow_faq_handlers()  # Регистрируем handlers для FAQ в Москве
+        register_dobrolyudova_handler()  # Регистрируем handlers для Dobrolyudova
+        register_ilyich_handler()  # Регистрируем handlers для Ilyich
+
     except Exception as e:
-        print(e)
+        logger.error(f"Ошибка: {e}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
